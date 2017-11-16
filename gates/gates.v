2259865a04724a1a4a1c5c440c561fc19b0ba920
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-11-16 16:13:33 +0000

    Fix issue with module NC2

--- gates/gates.v
+++ gates/gates.v
@@ -17,7 +17,7 @@ module NC2 (CK, RS, ST, A, B, QN, ENA, PRECAP);
     input CK, RS, ST, A, B, ENA;
     output QN, PRECAP;
 
-    C2 u1 (CK, RS, ST, ~A, ~B, QN, ENA);
+    C2 u1 (CK, RS, ST, ~A, ~B, QN, ENA, PRECAP);
 
 endmodule
 
@@ -28,16 +28,17 @@ module C2 (CK, RS, ST, A, B, Q, ENA, PRECAP);
     output reg Q;
     output reg PRECAP; // pre-capture or "next" value
 
-    always @(A, B, Q) begin
-             if ( A &&  B) PRECAP <= 1;
-        else if (~A && ~B) PRECAP <= 0;
-        else               PRECAP <= Q;
+    always @(A, B) begin
+             if ( A &&  B) PRECAP = 1;
+        else if (~A && ~B) PRECAP = 0;
+        else               PRECAP = Q;
     end
 
     always @(posedge CK or posedge RS or posedge ST) begin
-             if (RS)        Q <= 0;
-        else if (ST)        Q <= 1;
-        else if (CK && ENA) Q <= PRECAP;
+             if (RS)                    Q <= 0;
+        else if (ST)                    Q <= 1;
+        else if (CK && ENA &&  A &&  B) Q <= 1;
+        else if (CK && ENA && ~A && ~B) Q <= 0;
     end
 
 endmodule
